7b51a68bb4109189dd08c96fb00bb6d0023c65aa
Vinh Mao <vinhxmao87@gmail.com>
2021-03-02 19:57:03 -0800

    file initialization, basics added

---
+++ 1-basics/exercises.py
@@ -0,0 +1,16 @@
+print("Let's do this!")
+
+def print_nums():
+    print("1, 2, 3, 4, 5")
+
+def print_advice():
+    print("There's one thing every coder must understand:")
+    print("showing output with the print command")
+
+def main():
+    print_nums()
+    print_advice()
+    print()
+    print_advice()
+
+main()
