8e8c217d118be2d3b7298a5256ca51efed58f003
Vinh Mao <vinhxmao87@gmail.com>
2021-05-18 23:44:33 -0700

    new method print_squares added

--- 1-basics/exercises.py
+++ 1-basics/exercises.py
@@ -7,10 +7,20 @@ def print_advice():
     print("There's one thing every coder must understand:")
     print("showing output with the print command")
 
+def print_squares():
+    for i in range(1, 25):
+        print(i * i, end=" ")
+
+# def print_figure1():
+#     for i in range(7):
+#         for j in range(7):
+
 def main():
     print_nums()
     print_advice()
     print()
     print_advice()
+    # print_figure1()
+    print_squares()
 
 main()
