14eaadbad605de9b163d7abd777e1e3334faaeff
Vinh Mao <vinhxmao87@gmail.com>
2021-06-01 23:47:45 -0700

    check digit method completed

--- 2-conditionals/check_digits.py
+++ 2-conditionals/check_digits.py
@@ -1,13 +1,27 @@
+# This method 
+
 def main():
     print("This is a method.")
-    studentCode = int(input("Please enter a six-digit student code to determine the check digit."))
-    print(type(studentCode))
-    if not instanceof(studentCode, int):
+    studentCode = int(input("Please enter a six-digit code to determine the check digit."))
+    # print(type(studentCode))
+    if not isinstance(studentCode, int):
         raise TypeError("This value must be an integer!")
     if not number_of_digits(studentCode) == 6:
-        raise ValueError("Your student code must be 6 digits long!")
-    sum = 0   
+        raise ValueError("Your code must be 6 digits long!")
+    sum = 0
+    codeStr = str(studentCode)
+    # print(codeStr)
+    # print(isinstance(codeStr, str))
+    for i in range(len(codeStr)):
+        sum += (i + 1) * int(codeStr[i])
+        # print((i + 1) * int(codeStr[i]))
+    result = str(sum % 10)
+    codeStr += result
+    print("Your check digit is: ", result)
+    print("The full code is:", codeStr)
 
+# Checks the number of digits in the integer by performing
+# floor division repeatedly on the integer
 def number_of_digits(num):
     digits = 0
     while num > 0:
