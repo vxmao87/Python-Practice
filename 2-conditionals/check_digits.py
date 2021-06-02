7405869bf3c412c75a62e715ddb37ec67ea9ad6f
Vinh Mao <vinhxmao87@gmail.com>
2021-06-01 23:57:19 -0700

    comments added to check digit method

--- 2-conditionals/check_digits.py
+++ 2-conditionals/check_digits.py
@@ -1,27 +1,31 @@
-# This method 
+# This method accepts a number and reports the check digit for that number.
+# Check digits are important for verification and integrity purposes.
 
 def main():
-    print("This is a method.")
-    studentCode = int(input("Please enter a six-digit code to determine the check digit."))
-    # print(type(studentCode))
+    studentCode = int(input("Please enter a code to determine the check digit."))
+
+    # The input MUST be an integer or an error will be raised
     if not isinstance(studentCode, int):
         raise TypeError("This value must be an integer!")
-    if not number_of_digits(studentCode) == 6:
-        raise ValueError("Your code must be 6 digits long!")
+    
+    # The sum of all values using the formula in the for loop below
     sum = 0
+
+    # The inputted number is converted into a String under the variable name codeStr
     codeStr = str(studentCode)
-    # print(codeStr)
-    # print(isinstance(codeStr, str))
+
+    # The formula for calculating the check digit is as follows for
+    # a number with n digits:
+    # (n + 1)th digit = (1 x (1st digit) + 2 x (2nd digit) + ... + n x (nth digit)) % 10
     for i in range(len(codeStr)):
         sum += (i + 1) * int(codeStr[i])
-        # print((i + 1) * int(codeStr[i]))
     result = str(sum % 10)
-    codeStr += result
+
+    # Print the resulting check digit
     print("Your check digit is: ", result)
-    print("The full code is:", codeStr)
 
 # Checks the number of digits in the integer by performing
-# floor division repeatedly on the integer
+# floor division by 10 repeatedly on the integer
 def number_of_digits(num):
     digits = 0
     while num > 0:
