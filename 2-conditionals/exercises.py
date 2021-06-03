c91679297741fe504e4cb095b91d49bbf9854027
Vinh Mao <vinhxmao87@gmail.com>
2021-06-03 00:42:47 -0700

    conditional method added

--- 2-conditionals/exercises.py
+++ 2-conditionals/exercises.py
@@ -7,8 +7,18 @@ def fraction_sum(n):
         total += 1/i
     return total
 
+def longest_name(times):
+    if times < 0:
+        raise ValueError("Method will not run...")
+    longest_name = ""
+    for i in range(1, times + 1):
+        name = input("name? ")
+        if len(name) > len(longest_name):
+            longest_name = name
+    print(str.capitalize(longest_name),"'s name is longest")
 
 def main():
     print(fraction_sum(10))
+    longest_name(3)
 
 main()
