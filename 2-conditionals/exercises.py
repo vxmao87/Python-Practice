d9f7287de7284e3d4c9ca61aee96d4427c4e3f35
Vinh Mao <vinhxmao87@gmail.com>
2021-06-05 23:39:26 -0700

    small-large number method added

--- 2-conditionals/exercises.py
+++ 2-conditionals/exercises.py
@@ -17,8 +17,23 @@ def longest_name(times):
             longest_name = name
     print(str.capitalize(longest_name),"'s name is longest")
 
+def smallest_largest():
+    times = int(input("How many numbers do you want to enter? "))
+    largest = 0
+    smallest = 0
+    for i in range(1, times + 1):
+        user_num = int(input("Number", i))
+        if user_num > largest:
+            largest = user_num
+        elif user_num < smallest:
+            smallest = user_num
+    print("Smallest = ", smallest)
+    print("Largest = ", largest)
+
+
 def main():
     print(fraction_sum(10))
     longest_name(3)
+    smallest_largest()
 
 main()
