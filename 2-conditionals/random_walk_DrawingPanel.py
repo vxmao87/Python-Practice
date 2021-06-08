6617ea6aac4bf28c4b2939b53a307d899324cf96
Vinh Mao <vinhxmao87@gmail.com>
2021-06-07 18:19:58 -0700

    random walk method finished

---
+++ 2-conditionals/random_walk_DrawingPanel.py
@@ -0,0 +1,25 @@
+from DrawingPanel import *
+import random
+import time
+
+def main():
+    panel = DrawingPanel(500, 500)
+    STEPS = 50
+    x_pos = 250
+    y_pos = 250
+    panel.draw_rect(x_pos, y_pos, 1, 1)
+
+    for i in range(STEPS):
+        # time.sleep(1)
+        pos = random.randrange(1, 5, 1)
+        if pos == 1:
+            y_pos += 5
+        elif pos == 2:
+            y_pos -= 5
+        elif pos == 3:
+            x_pos -= 5
+        else:
+            x_pos += 5
+        panel.draw_rect(x_pos, y_pos, 1, 1)
+
+main()
