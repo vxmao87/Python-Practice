23ccf9a9c1fbfd1948b440d8c68df98bf6676dd0
Vinh Mao <vinhxmao87@gmail.com>
2021-06-08 00:11:05 -0700

    document-reading method completed

--- 3-files/word_counter/script.py
+++ 3-files/word_counter/script.py
@@ -3,6 +3,7 @@
 
 import os.path
 
+# Boilerplate code for verifying readable files
 def prompt_for_file(message):
     user_input = input(message)
     filename = "3-files/word_counter/{}".format(user_input)
@@ -13,13 +14,29 @@ def prompt_for_file(message):
 
 def main():
     filename = prompt_for_file("File to open? ")
-    word_count = 0
+
+    # Counters for lines, words and characters
     line_count = 0
+    word_count = 0
     char_count = 0
+
     with open(filename) as file:
         for line in file:
-            line_count += 1
+
+            # Removes all "newline" instances so that the input is all one line
+            line = line.strip("\n")
+
+            # Returns a list of words in the given line by disregarding whitespace
+            words = line.split()
+
+            line_count += 1            
+            word_count += len(words)
+            char_count += len(line)
+
+        # Prints the number of lines, words and characters in the document    
         print("Line count: ", line_count)
+        print("Word count: ", word_count)
+        print("Character count: ", char_count)
 
 
 main()
