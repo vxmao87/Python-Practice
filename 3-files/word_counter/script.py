cb31f52159592d376f04d0da44d80759f39473cb
Vinh Mao <vinhxmao87@gmail.com>
2021-06-07 23:43:53 -0700

    document-reading method implemented, working on word and character count portions

--- 3-files/word_counter/script.py
+++ 3-files/word_counter/script.py
@@ -1,7 +1,25 @@
 # This method reads a file and prints the number
 # of words, lines and characters in the given file.
 
+import os.path
+
+def prompt_for_file(message):
+    user_input = input(message)
+    filename = "3-files/word_counter/{}".format(user_input)
+    while not os.path.isfile(filename):
+        print("File not found. Try again.")
+        filename = input(message)
+    return filename
+
 def main():
-    print("This is a method.")
+    filename = prompt_for_file("File to open? ")
+    word_count = 0
+    line_count = 0
+    char_count = 0
+    with open(filename) as file:
+        for line in file:
+            line_count += 1
+        print("Line count: ", line_count)
+
 
 main()
