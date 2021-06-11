d41d305608119412a0184fb00748e665eb9f16c8
Vinh Mao <vinhxmao87@gmail.com>
2021-06-10 23:52:48 -0700

    custom story method created

---
+++ 3-files/make_your_own_story/script.py
@@ -0,0 +1,8 @@
+# This method takes in words from the user and creates
+# a story based on their input. The file uses word
+# placeholders to create the story.
+
+def main():
+    print("method")
+
+main()
