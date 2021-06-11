b71e083f1dc550c5325af69ce08f57af51b6a7dc
Vinh Mao <vinhxmao87@gmail.com>
2021-06-10 23:54:34 -0700

    boilerplate code added

--- 3-files/make_your_own_story/script.py
+++ 3-files/make_your_own_story/script.py
@@ -2,6 +2,16 @@
 # a story based on their input. The file uses word
 # placeholders to create the story.
 
+# Boilerplate code for verifying readable files
+def prompt_for_file(message):
+    user_input = input(message)
+    filename = "3-files/make_your_own_story/{}".format(user_input)
+    while not os.path.isfile(filename):
+        print("File not found. Try again.")
+        user_input = input(message)
+        filename = "3-files/make_your_own_story/{}".format(user_input)
+    return filename
+
 def main():
     print("method")
 
