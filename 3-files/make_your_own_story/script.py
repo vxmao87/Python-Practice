2a9f85de8d0b1080b3fbd87bdecc2e83392c20f8
Vinh Mao <vinhxmao87@gmail.com>
2021-06-12 00:21:41 -0700

    story method start

--- 3-files/make_your_own_story/script.py
+++ 3-files/make_your_own_story/script.py
@@ -2,17 +2,16 @@
 # a story based on their input. The file uses word
 # placeholders to create the story.
 
-# Boilerplate code for verifying readable files
-def prompt_for_file(message):
-    user_input = input(message)
-    filename = "3-files/make_your_own_story/{}".format(user_input)
-    while not os.path.isfile(filename):
-        print("File not found. Try again.")
-        user_input = input(message)
-        filename = "3-files/make_your_own_story/{}".format(user_input)
-    return filename
+def print_intro():
+    print("Create your own story by typing in your own words!")
+    print("You won't know what the story says, so you'll be in for a surprise!")
+
+def write_story():
+    adjective_1 = "red"
+    # with ("3-files/make_your_own_story/story.txt", "w") as outfile:
 
 def main():
-    print("method")
+    print_intro()
+    write_story()
 
 main()
