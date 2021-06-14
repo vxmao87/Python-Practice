5604c637417835deed3ca777e142972aa906ce27
Vinh Mao <vinhxmao87@gmail.com>
2021-06-13 17:27:10 -0700

    Python list exercises

---
+++ 4-lists/exercises.py
@@ -0,0 +1,57 @@
+def list_range(lis):
+    # min = lis[0]
+    # max = lis[0]
+    # for i in range(len(lis)):
+    #     if lis[i] > max:
+    #         max = lis[i]
+    #     if lis[i] < min:
+    #         min = lis[i]
+    # return max - min + 1
+    return max(lis) - min(lis) + 1
+
+def is_sorted(lis):
+    if len(lis) == 1:
+        return True
+    current_num = lis[0]
+    for i in range(len(lis)):
+        if lis[i] < current_num:
+            return False
+        else:
+            current_num = lis[i]
+    return True
+
+def mode(lis):
+    tally = [0] * max(lis)
+    for i in range(len(lis)):
+        tally[lis[i] - 1] += 1
+    return tally.index(max(tally)) + 1
+
+def median(lis):
+    new_lis = sorted(lis)
+    return new_lis[len(new_lis) // 2]
+
+def price_is_right(bids, correct_bid):
+    best_bid = -1
+    for i in range(len(bids)):
+        if bids[i] <= correct_bid and bids[i] > best_bid:
+            best_bid = bids[i]
+    return best_bid
+
+def main():
+
+    print(list_range([36, 12, 25, 19, 46, 31, 22]))
+
+    print(is_sorted([16.1, 12.3, 22.2, 14.4]))
+    print(is_sorted([1.5, 4.3, 7.0, 19.5, 25.1, 46.2]))
+    print(is_sorted([23]))
+    print(is_sorted([23, 25]))
+
+    print(mode([27, 15, 15, 11, 27]))
+
+    print(median([5, 2, 4, 17, 55, 4, 3, 26, 18, 2, 17]))
+    print(median([42, 37, 1, 97, 1, 2, 7, 42, 3, 25, 89, 15, 10, 29, 27]))
+
+    bids = [200, 300, 250, 1, 950, 40]
+    print(price_is_right(bids, 280))
+
+main()
