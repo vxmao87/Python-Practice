02581c540fcc01b543c231b6906e6289cb0dc759
Vinh Mao <vinhxmao87@gmail.com>
2021-06-15 17:08:11 -0700

    tic-tac-toe method initialization

---
+++ 4-lists/tic_tac_toe.py
@@ -0,0 +1,13 @@
+# Play a game of tic-tac-toe with a friend using this program.
+
+def print_intro():
+    pass
+
+def play_game():
+    pass
+
+def main():
+    print_intro()
+    play_game()
+
+main()
