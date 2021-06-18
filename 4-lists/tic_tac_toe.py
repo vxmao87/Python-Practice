2da1ae29c97a04c03e3be3641d0bda771cb317cb
Vinh Mao <vinhxmao87@gmail.com>
2021-06-17 20:23:49 -0700

    tic-tac-toe project - some conditionals formed

--- 4-lists/tic_tac_toe.py
+++ 4-lists/tic_tac_toe.py
@@ -3,12 +3,17 @@
 def print_intro():
     pass
 
-def wins(grid):
-    if (grid[0][0] == grid[0][1] and grid[0][1] == grid[0][2]) or (grid[1][0] == grid[1][1] and grid[1][1] == grid[1][2]) or (grid[2][0] == grid[2][1] and grid[2][1] == grid[2][2]):
+def wins(grid, player):
+    icon = "x"
+    if player == "1":
+        icon = "x"
+    else:
+        icon = "o"
+    if (grid[0][0] == grid[0][1] and grid[0][1] == grid[0][2] and grid[0][0] == icon) or (grid[1][0] == grid[1][1] and grid[1][1] == grid[1][2] and grid[1][0] == icon) or (grid[2][0] == grid[2][1] and grid[2][1] == grid[2][2] and grid[2][0] == icon):
         return True
-    elif (grid[0][0] == grid[1][0] and grid[1][0] == grid[2][0]) or (grid[0][1] == grid[1][1] and grid[1][1] == grid[2][1]) or (grid[0][2] == grid[1][2] and grid[1][2] == grid[2][2]):
+    elif (grid[0][0] == grid[1][0] and grid[1][0] == grid[2][0] and grid[0][0] == icon) or (grid[0][1] == grid[1][1] and grid[1][1] == grid[2][1] and grid[0][1] == icon) or (grid[0][2] == grid[1][2] and grid[1][2] == grid[2][2] and grid[0][2] == icon):
         return True
-    elif (grid[0][0] == grid[1][1] and grid[1][1] == grid[2][2]) or (grid[0][2] == grid[1][1] and grid[1][1] == grid[3][0]):
+    elif (grid[0][0] == grid[1][1] and grid[1][1] == grid[2][2] and grid[0][0] == icon) or (grid[0][2] == grid[1][1] and grid[1][1] == grid[2][0] and grid[0][2] == icon):
         return True
     else:
         return False
@@ -16,6 +21,15 @@ def wins(grid):
 def play_game():
     grid = [["_"] * 3, ["_"] * 3, ["_"] * 3]
     print(grid)
+
+    print(wins(grid, "1"))
+    print(wins(grid, "2"))
+    while not wins(grid, "1") and not wins(grid, "2"):
+        if not wins(grid, "1"):
+            print("Type in two numbers, where the first number is the row you want to place your icon,")
+            player_input = input("and the second number is the column you want to place your icon (1-3): ")
+            
+
 
 def main():
     print_intro()
