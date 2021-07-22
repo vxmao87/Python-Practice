f227432f61d896ce7a26a9f6cb8844844abce54e
Vinh Mao <vinhxmao87@gmail.com>
2021-07-21 22:07:56 -0700

    first recursion exercise implemented

---
+++ 6-recursion/exercises.py
@@ -0,0 +1,17 @@
+# Prints 2^n stars depending on the value of n
+def star_string(n):
+    if n < 0:
+        raise ValueError("This number is invalid. Try again!")
+    if n == 0:
+        return "*"
+    else:
+        return star_string(n - 1) + star_string(n - 1)
+
+def main():
+    print(star_string(4))   # Prints 16 stars
+    print(star_string(3))   # Prints 8 stars
+    print(star_string(2))   # Prints 4 stars
+    print(star_string(1))   # Prints 2 stars
+    print(star_string(0))   # Prints 1 star
+
+main()
