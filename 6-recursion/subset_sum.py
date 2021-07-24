3267c3ced5cf155520cbaf15ab5e2a9127a1ca79
Vinh Mao <vinhxmao87@gmail.com>
2021-07-24 00:23:30 -0700

    subset method progress

--- 6-recursion/subset_sum.py
+++ 6-recursion/subset_sum.py
@@ -1,9 +1,15 @@
-def subset_sum(list, target):
-    pass
+# Finds the first subset in a list that adds up to the given target value
+def subset_sum(list, target, length):
+
+    # Traverse between two indexes
+    for i in range(length):
+        current_sum = list[i]
+        j = i + 1
 
 def main():
     list = [4, 6, 3, 2, 9, 3, 1, 6]
+    length = len(list)
     target = 13
-    subset_sum(list, target)
+    subset_sum(list, target, length)
 
 main()
